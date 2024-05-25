197055ed7eb57db02783dfa4a95065424bcd6467
Mark de Neut Brossois <mark.dnb@live.nl>
2024-05-25 11:02:16 +0200

    Added delay to all objects

--- playground/mocking/index.ts
+++ playground/mocking/index.ts
@@ -5,18 +5,19 @@ export const getMocks = () => {
   for (const mock of mocks.mocks) {
     if (mock) {
       // Split to have groups of the mocked responses
-      const mockGroup = { groupName: mock.groupName, mockList: [] }
-      for (const mockList of mock.mockList) {
+      const mockGroup = { groupName: mock.groupName, requests: [] }
+      for (const request of mock.requests) {
         // Retrieve the json file with the response header values
-        import(mockList).then((files) => {
+        import(request).then((files) => {
           const apiRequests = { ...files }
           for (const responseFile in apiRequests.responses) {
             // Retrieve the json file with the all the selectable response values
             import(apiRequests.responses[responseFile]).then(responseData =>
-              apiRequests.responses[responseFile] = responseData,
+              // structuredClone is needed, so the duration update on the mocking route, won't change all durations from the same json file
+              apiRequests.responses[responseFile] = structuredClone(responseData),
             )
           }
-          mockGroup.mockList.push(apiRequests)
+          mockGroup.requests.push(apiRequests)
         })
       }
       resultMocks.push(mockGroup)
