28a2c2ff356120bccfd2ec8ea748f72ec4d04156
Mark de Neut Brossois <mark.dnb@live.nl>
2025-01-03 13:38:12 +0100

    Fixed regex

--- src/runtime/event-handlers/api.ts
+++ src/runtime/event-handlers/api.ts
@@ -5,7 +5,7 @@ import type { MockResponses } from '../types/mock-list'
 export const apiEvent = (event: H3Event<EventHandlerRequest>, apiUrl: string, responses: { [key: string]: MockResponses }) => {
   const responseKeys = Object.keys(responses)
   const url = `${event.method}_${apiUrl + event.path}`
-  const activeKey = responseKeys.find(key => new RegExp(`\\b(${key})\\`, 'g').test(url))
+  const activeKey = responseKeys.find(key => new RegExp(`\\b(${key})\\b`, 'g').test(url))
   if (!activeKey) {
     return
   }
